5e4024888ad06e080df773ed6e03185243d502a5
muirenee <67049659+muirenee@users.noreply.github.com>
2026-09-14 19:50:13 +0200

    chore: add Prisma configuration

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'packages/database/prisma/schema.prisma',
+  migrations: {
+    path: 'packages/database/prisma/migrations',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+});
